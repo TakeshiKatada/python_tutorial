78b84730a6d42857259b66fdb0cc7ffeb296a306
片田 猛士 <katada-t@hi-ad.jp>
2019-02-21 11:20:35 +0900

    プログラム案その3

---
+++ x3_03.py
@@ -0,0 +1,11 @@
+def num(n):
+    a = 1
+    while a < n:
+        if a % 3 == 0:
+            print a,"*"
+            a = a + 1
+        else:
+            print a
+            a = a + 1
+
+num(41)
